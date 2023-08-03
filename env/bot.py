431389eedcfbf8ee9cba3d5aa5f44427160ad7f2
Nolan <nolanroth1@gmail.com>
2023-08-02 21:22:28 -0400

    attempt at fetching top loan offer

--- env/bot.py
+++ env/bot.py
@@ -2,6 +2,8 @@
 import discord
 import random
 import bestloan
+import requests
+import json
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -28,7 +30,14 @@ async def on_message(message):
     if message.content.startswith('$loans'):
         if len(message.content.split())>1:
             collection = message.content.split()[1]
-            await message.channel.send("Loans for " + collection)
+            BASE_URL = "https://dev-skillet-backend-rng3tbs6qq-uc.a.run.app/topLoanOfferForCollection"
+
+            collection_address = "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d"
+            response = requests.get(f"{BASE_URL}?collectionAddress={collection_address}")
+
+            offer = json.loads(response.text)
+            print(offer)
+            #await message.channel.send("Loans for " + collection)
             #print(message.content.split())
     #if message.content.startswith('How are you today?'):
     #    await message.channel.send('Average at best') hello 
@@ -39,4 +48,4 @@ async def on_message(message):
     if message.content.startswith('Ethereum loan'):
        await message.channel.send('The best avaiable loan for Etherem: ' )#inputbestloan
 
-client.run('MTExODM3MTEzMTQ2OTA5MDgzNg.Gd2GV4.SeChrqtSmFcxOVT34fmK6l85GeYZ0Ne3_MHVrw')
+client.run('MTExODM3MTEzMTQ2OTA5MDgzNg.GYZpMo.O9NGiLD4HcsssiNs2-tUibjBR2RDD0PICkp3r8')
